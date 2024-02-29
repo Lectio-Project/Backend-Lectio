f57b2a3ab7c5c1487d3aeac22f5b80b51b0b72ce
alissonromaosantos <eualissonromaosantos@gmail.com>
2024-02-29 18:01:22 -0300

    feat: Autenticação e autorização de usuário

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,14 +10,20 @@ import {
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
+import { LoginDto } from './dto/login-user.dto';
 
-@Controller('users')
+@Controller('')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Post('sign-in')
+  login(@Body() loginDto: LoginDto) {
+    return this.usersService.login(loginDto);
   }
 
   @Get()
