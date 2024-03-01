14bde092d0c64c19bf4f51a341e50c447476b77b
alissonromaosantos <eualissonromaosantos@gmail.com>
2024-03-01 17:25:00 -0300

    feat: Crud de gêneros de livros

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { GenresModule } from './genres/genres.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, AuthModule],
+  imports: [PrismaModule, UsersModule, AuthModule, GenresModule],
   controllers: [],
   providers: [],
 })
