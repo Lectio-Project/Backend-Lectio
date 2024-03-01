b3da60602c6a96f602535ec2ecaa0f245fb786ca
alissonromaosantos <eualissonromaosantos@gmail.com>
2024-03-01 17:25:00 -0300

    feat: Crud de gêneros de livros

---
+++ src/genres/genres.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { GenresService } from './genres.service';
+import { CreateGenreDto } from './dto/create-genre.dto';
+import { UpdateGenreDto } from './dto/update-genre.dto';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
+
+@UseGuards(AuthGuard)
+@Controller('genres')
+export class GenresController {
+  constructor(private readonly genresService: GenresService) {}
+
+  @Post()
+  create(@Body() createGenreDto: CreateGenreDto) {
+    return this.genresService.create(createGenreDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.genresService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.genresService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateGenreDto: UpdateGenreDto) {
+    return this.genresService.update(id, updateGenreDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.genresService.remove(id);
+  }
+}
