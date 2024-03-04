dc94612922b1948e970c18d9dc2b570825319c4f
staviasz <erickesc21@hotmail.com>
2024-03-04 16:13:42 -0300

    adicionado filtros de pesquisa em authors

--- src/author/dto/update-author.dto.ts
+++ src/author/dto/update-author.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsOptional } from 'class-validator';
+import { IsArrayOfIdStringsOrIdString } from 'src/utils/validators/IsArrayOfIdStringsOrIdString';
 import { CreateAuthorDto } from './create-author.dto';
 
-export class UpdateAuthorDto extends PartialType(CreateAuthorDto) {}
+export class UpdateAuthorDto extends PartialType(CreateAuthorDto) {
+  @IsOptional()
+  @IsArrayOfIdStringsOrIdString()
+  usersId?: Array<string>;
+}
