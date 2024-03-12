68af92a398cfb39de2cbbf061cbaebfbebb26295
staviasz <erickesc21@hotmail.com>
2024-03-12 10:20:20 -0300

    resolvido os bugs da validação de senha e do nome completo

--- src/utils/validators/IsValidPassword.ts
+++ src/utils/validators/IsValidPassword.ts
@@ -8,7 +8,7 @@ import {
 export class IsValidPasswordContraint {
   validate(password: string): boolean {
     const re = new RegExp(
-      '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$',
+      '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&.])[A-Za-z\\d@$!%*?&.]{8,}$',
     );
     return re.test(password);
   }
